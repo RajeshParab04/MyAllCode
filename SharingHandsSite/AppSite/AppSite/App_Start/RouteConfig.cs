48f51a59fd88379efa6a6f8fc19d005868732a06
Rajesh <rajesh.parab@crmnext.in>
2018-03-13 13:07:18 +0530

    Done changes for call site using domain name

--- SharingHandsSite/AppSite/AppSite/App_Start/RouteConfig.cs
+++ SharingHandsSite/AppSite/AppSite/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace AppSite
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "app", action = "SharingHands", id = UrlParameter.Optional }
             );
         }
     }
